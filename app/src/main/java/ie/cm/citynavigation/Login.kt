4ebd0e963f0fd71bb09bb99bbf5eeefac9b9238d
José Ferreira <jose.f.a@ipvc.pt>
2021-03-05 19:18:19 +0000

    Login page done

---
+++ app/src/main/java/ie/cm/citynavigation/Login.kt
@@ -0,0 +1,26 @@
+package ie.cm.citynavigation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //  Temp code for navigation between activities
+        val button: Button = findViewById(R.id.loginButton)
+        button.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+        val withoutLogin: TextView = findViewById(R.id.withoutLogin)
+        withoutLogin.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
