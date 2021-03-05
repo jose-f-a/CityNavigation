eea2a1e0107dc8811274ce65c4c6889f27d05ff9
José Ferreira <jose.f.a@ipvc.pt>
2021-03-05 18:40:49 +0000

    Initial commit

---
+++ app/src/main/java/ie/cm/citynavigation/MainActivity.kt
@@ -0,0 +1,11 @@
+package ie.cm.citynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
