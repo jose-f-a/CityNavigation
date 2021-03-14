32fe87c5a1472fc09690aa587220ac3909f5252f
José Ferreira <jose.f.a@ipvc.pt>
2021-03-14 19:17:16 +0000

    mapa na atividade Map c/ chave da API

--- app/src/main/java/ie/cm/citynavigation/Map.kt
+++ app/src/main/java/ie/cm/citynavigation/Map.kt
@@ -3,15 +3,23 @@ package ie.cm.citynavigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class Map : AppCompatActivity() {
+class Map : AppCompatActivity(), OnMapReadyCallback {
 
   private lateinit var tempbtn: Button
+  private lateinit var mMap: GoogleMap
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_map)
 
+    //Toolbar
     setSupportActionBar(findViewById(R.id.homeToolbar))
     supportActionBar?.apply {
       setDisplayHomeAsUpEnabled(true)
@@ -19,11 +27,26 @@ class Map : AppCompatActivity() {
       setDisplayShowTitleEnabled(true)
     }
 
+    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    val mapFragment = supportFragmentManager
+      .findFragmentById(R.id.mapp) as SupportMapFragment
+    mapFragment.getMapAsync(this)
+
+    //Butao temporario
     tempbtn = findViewById(R.id.tempbtn)
     val reportFragment = ReportFragment()
 
     tempbtn.setOnClickListener {
       reportFragment.show(supportFragmentManager, "aaa")
     }
+  }
+
+  override fun onMapReady(googleMap: GoogleMap) {
+    mMap = googleMap
+
+    // Add a marker in Sydney and move the camera
+    val ESGT = LatLng(41.693408, -8.846684)
+    mMap.addMarker(MarkerOptions().position(ESGT).title("Marker in ESTG"))
+    mMap.moveCamera(CameraUpdateFactory.newLatLng(ESGT))
   }
 }
